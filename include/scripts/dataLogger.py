781f8ea5d59938b06605fd851a1d1eed80f554c6
Ashwn Dar <amdara14@gmail.com>
2020-05-07 10:01:45 -0700

    Fixed string to float conversion from the stream file

--- include/scripts/dataLogger.py
+++ include/scripts/dataLogger.py
@@ -1,32 +1,31 @@
 import matplotlib.pyplot as plt
 
 class Logger:
-    x = 10
     def __init__(self, file, y_axis, streamArg):
         self.file = file
         self.y_axis = y_axis
         self.streamArg = streamArg
         self.y = []
-    
-    def visualPlot(self, space):
+        
+    def visualPlot(self, space, appearance):
         fstream = open(self.file, self.streamArg)
         stream = fstream.readlines()
         for i in stream: 
-            self.y.append(i)
+            self.y.append(float(i))
         self.values = self.y
         plt.yscale("linear")
         self.space = space
+        self.appearance = appearance
         plt.axis(self.space)
-        plt.plot([1, 2, 3], self.values, 'ro')
+        plt.plot(self.values, self.appearance)
         plt.ylabel(self.y_axis)
         plt.show()
 
 powerLog = Logger("include\data\powerStreamData.txt", "power(rpm)", "r")
 
 def main():
-    powerLog.visualPlot([0, 4, 0, 4])
-
-    print(x)
+    
+    powerLog.visualPlot([0, 10, 0, 200], 'p')
  #   logger = open("include\data\powerStreamData.txt", "r")
  #   #if logger.mode = "r":
  #   stream = logger.readlines()
