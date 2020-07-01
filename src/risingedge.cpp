27f42627f069effbf6d5bc3d1efa13fcb975e23a
BuildTools <unconfigured@null.spigotmc.org>
2020-07-01 14:53:23 -0700

    fix commit

--- src/risingedge.cpp
+++ src/risingedge.cpp
@@ -1,21 +1,21 @@
 void opcontrol() {
     pros::Controller master(CONTROLLER_MASTER); //I always name the controller "master" don't judge
-    int count;
-    bool btnA;
+    int count = 0;
+    bool btnA = false;
     int n = 150;
-    int target;
+    int target = 0;
     
     while (true) { //rising edge 
         if(master.get_digital(DIGITAL_A) && !btnA){  //if button is pressed and the button hasn't been pressed before add one to "count"
             count++;
-            if (count == 1) {
-                target = n
+            if (count %2 == 1) {
+                target = target
             }
-            if (count == 2) {
-                target = n % 150
+            if (count %2 == 0) {
+                target = 150
             }
-            if (count == 3) {
-                target = n
+            if (count %2 == 1/3) {
+                target = target
                 count = 1
             }
         
