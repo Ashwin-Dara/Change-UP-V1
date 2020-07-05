cf7fe83991d9a91e374d72292d1770222356f03d
Ashwn-Dar <48425033+Ashwn-Dar@users.noreply.github.com>
2020-07-04 21:00:41 -0700

    Update robot.h

--- include/robot.h
+++ include/robot.h
@@ -1,8 +1,8 @@
-____________________________________________ header file
+//deleted the line that you had
 #ifndef ROBOT_H
 #define ROBOT_H
 //code 
-#endif
+//#endif (ORIGINAL) (it should be at the end of the file)
 
 extern pros::Controller master(CONTROLLER_MASTER);
 extern pros::Motor rightMotorOne;
@@ -12,3 +12,5 @@ extern pros::Motor leftMotorTwo;
 extern pros::Motor intakeOne;
 extern pros::Motor intakeTwo;
 extern pros::Motor flywheel;
+
+#endif //EDIT: good stuff just the endif goes at the end of the file 
