a95e4c3c6b09db689fd6cfbd3e7859efcc6b0530
Ashwn-Dar <48425033+Ashwn-Dar@users.noreply.github.com>
2020-07-04 21:05:09 -0700

    Update risingedge.cpp

--- src/risingedge.cpp
+++ src/risingedge.cpp
@@ -7,20 +7,19 @@ void opcontrol() {
     
     while (true) { //rising edge 
         if(master.get_digital(DIGITAL_A) && !btnA){  //if button is pressed and the button hasn't been pressed before add one to "count"
-            count++;
-            if (count %2 == 1) {
+            count++; //MISSING SEMI-COLONS ON LINE 12, 15, and 18
+            if (count %2 == 1) { //elaborate on the logic behind "target = target"
                 target = target
             }
             if (count %2 == 0) {
                 target = 150
             }
-            if (count %2 == 1/3) {
+            if (count %2 == 1/3) { //when will the remainder by 1/3? 
                 target = target
                 count = 1
             }
-        
 
-        }
+        } //check the brackets as well 
         btnA = master.get_digital(DIGITAL_A); //defining variable as button press
         pros::delay(20);
     }
