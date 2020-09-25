2ff07d9f5eac15f4d50a2d980b53edc62315e408
Niwhsa Arad <48425033+Ashwn-Dar@users.noreply.github.com>
2020-09-25 12:28:57 -0700

    Create GeneralDebugger.cpp

---
+++ src/logger/GeneralDebugger.cpp
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string>
+
+class Llemu{
+  public:
+    static bool heaviside; 
+    
+    static void init(){
+      pros::lcd::initialize(); 
+    }
+    
+    static void debugDrive(const Drive &drive){
+    //debug the chassis, PID, autonomous, intake, uptake
+    }
+    
+    static void debugAutonomous(const Drive &drive);
+    //show the error, position, power, and etc.
+    
+    static void debugIntake(const Intake &intake); 
+    //display the true velocity of each component of the subsystem
+    
+
+};
