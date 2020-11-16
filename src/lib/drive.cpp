c74c21eda596affeaee3966f30b398fb71c9e7ed
Niwhsa Arad <48425033+Ashwn-Dar@users.noreply.github.com>
2020-11-16 09:05:35 -0800

    Update drive.cpp
    - corrected the class implementation for drive

--- src/lib/drive.cpp
+++ src/lib/drive.cpp
@@ -10,17 +10,18 @@ float powerSpline(float axisInput){
 }
 
 void operatorControl(bool usingSpline = false){
+    
     int rightAxis = joystick.get_analog(ANALOG_RIGHT_Y);
     int leftAxis = joystick.get_analog(ANALOG_LEFT_Y);
     int rightPower, leftPower;
     
     if(usingSpline){
         if(abs(joystick.get_analog(ANALOG_RIGHT_Y)) > 2)
-            rightPower = (powerSpline(rightAxis) - powerSpline(0));
+            rightPower = 95 * (powerSpline(rightAxis) - powerSpline(0));
         if(abs(joystick.get_analog(ANALOG_LEFT_Y)) > 2)
-            leftPower = (powerSpline(leftAxis) - powerSpline(0));
-        else leftPower = 0; 
-            driveVoltageAssign(rightPower*95, leftPower*95);
+            leftPower = 95 * (powerSpline(leftAxis) - powerSpline(0));
+        else leftPower = 0;
+            driveVoltageAssign(rightPower, leftPower);
     } 
     else if(!usingSpline){   
         rightPower = rightAxis * 110; 
@@ -29,41 +30,38 @@ void operatorControl(bool usingSpline = false){
         }
     }
     
-    //WILL FORMAT LATER // TEMP
-    class Drive : public PID : public LinearOdometry{ 
-        public: 
-            int16_t rightSet, leftSet; 
-            int16_t thetaSet; 
-            Drive() : rightSet(0), leftSet(0), thetaSet(0);
-            void moveRelative(int linearTarget){
-                rightSet += linearTarget;
-                leftSet += linearTarget;
-            }
-            void turnRelative(int degrees){
-                thetaSet += degrees; 
-            }
-            void iterate(){
-                int rightPidPower = calc(rightSet, rightMotor.get_value()); 
-                int leftPidPower = calc(leftSet, leftMotor.get_value()); 
-                
-                pros::delay( 25 ); 
-                
-                if(slew) { 
-                    int newRPid = calc(rightSet, rightMotor.get_value() ); 
-                    int newLPid = calc(leftSet, leftMotor.get_value() ); 
-                    if ( abs( newRPid - rightPidPower) > SLEW_LIMIT) 
-                        rightPidPower += sgn( newRPid - rightPidPower ) * DRIVE_KA;
-                        else rightPidPower = newRPid; 
-                    if ( abs( newLPid - leftPidPower) > SLEW_LIMIT)
-                        leftPidPower += sgn( newLPid - leftPidPower) * DRIVE_KA; 
-                        else leftPidPower = newLPid; 
-                }
-                
-                  int thetaCorrect = THETA_GAIN * calc(thetaSet, IMU.get_yaw());
-                  driveMove( rightPidPower + thetaCorrect, leftPidPower - thetaCorrect); 
-
-            }
+    Drive::Drive() : rightSet(0), leftSet(0), thetaSet(0){}
+    
+    Drive::~Drive() = default; 
+    
+    void Drive::moveRelative(int linearTarget) {
+        rightSet += linearTarget; 
+        leftSet  += linearTarget; 
+    }
+    
+    void Drive::turnRelative(int degrees){ 
+        thetaSet += degrees;
+    }
+    
+    void Drive::iterate(){
+        int rightPidPower = calc(rightSet, rightMotor.get_value()); 
+        int leftPidPower = calc(leftSet, leftMotor.get_value()); 
+        pros::delay(25); 
         
-    };
+        if(slew) { 
+            int newRPid = calc(rightSet, rightMotor.get_value() ); 
+            int newLPid = calc(leftSet, leftMotor.get_value() ); 
+            
+            if ( abs( newRPid - rightPidPower) > SLEW_LIMIT) 
+                rightPidPower += sgn( newRPid - rightPidPower ) * DRIVE_KA;
+            else rightPidPower = newRPid; 
+            
+            if ( abs( newLPid - leftPidPower) > SLEW_LIMIT)
+                leftPidPower += sgn( newLPid - leftPidPower) * DRIVE_KA; 
+            else leftPidPower = newLPid;             
+        }
+        int thetaCorrect = THETA_GAIN * calc(thetaSet, IMU.get_yaw()); 
+        driveMove(rightPidPower + thetaCorrect, leftPidPower - thetaCorrect); 
+    }
 
 }
