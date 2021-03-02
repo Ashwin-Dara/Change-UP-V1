266d08a099e39898e171262e1298233cdce8f3fa
Niwhsa Arad <48425033+Ashwn-Dar@users.noreply.github.com>
2021-03-02 12:47:25 -0800

    Update main.cpp
    Updating the main.cpp with the current auton function that is written by Aayush. 
    
    Movement functions are the same as January, unchanged from Andres's slew and PID functions. However, the kS for the straightening code was increased after the skills run in late Feb. 
    
    @Toyler would appreciate if you review the commit.

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,143 @@ void competition_initialize() {}
  * from where it left off.
  */
 
-void autonomous() {}
+
+void new_programming_skills(){
+
+			/* 
+			Double Move function is pretty much the same thing as the regular move function. 
+			Parameters: int target, int secondVelocityBound (leave it at 50), velocity, velocity (make them the same number), float slewRate
+
+			We found that typically a slew rate of 0.9 is better for backing away from goals/intaking balls (with a velocity of 25-30)
+			while a rate of 1.3 and max velocity of 105 would be ideal for far movements
+
+			To go towards the goals, I personally used "timeMove(int speed, int MS)" since no PID is needed because the goals re-align us. This was 
+			made with the intention of saving time. 
+
+			Overall, there are ~4 goals still pending, but they should not be too diffucult to complete. 
+			*/
+			
+			bool descore_corner_goals = true; //If this value is true, it will run the code within the autonomous for descoring the corner goals
+											  //If false, then the descoring procedures will not occur 
+
+
+			//First movement: go forward and intake the ball 
+			index(); 
+			doubleMove(450, 50, true, 105, 105, 1.3); //used to be 0.9 for the slew rate
+			
+
+			//Second movement: turn towards the goal and stop intaking after turning 			
+			turn(125, true, 1, true);
+			sis();  
+
+			
+			
+
+			//Move forward towards the first corner goal
+			timeMove(70, 700); //going at a velocity of 70 for 700 MS
+			timeMove(40, 480); 
+			pros::delay(100); //Some time to rest to prevent bounce? (this works)
+			autonASI(); //shoot the ball 
+
+
+			//Move backwards and turn away
+			doubleMove(-300, 50, true, 100, 100, 0.9);
+			turn(146, true, 1, true);
+
+			
+			//Go forward, turn, and now you're in position for the second goal 
+			doubleMove(975, 50, true, 100, 100, 1.3);
+			turn(-93, true, 1, true); 
+ 
+
+			//Go into the second goal and score
+			timeMove(45, 950); //need to modify this 
+			timeMove(8, 100);
+			/*index(); //intaking the balls to descore while in position to shoot
+			delay(750);
+			sis();*/
+			autonASNI();  //NO INDEXING 
+			
+
+			//move backwards and turn to face the ball next to the corner goal 
+			doubleMove(-200, 50, true, 100, 100, 0.9);
+			//descoreFunc();
+			turn(87, true, 1, true);
+
+
+			//Go forward and then go forward once again but slower
+			doubleMove(400, 50, true, 105, 105, 1.3);
+			index(); 
+			doubleMove(600, 50, true, 25, 25, 0.9);
+			pros::delay(50); //Give it some time to rest before cutting the indexing 
+			sis(); 
+
+
+			//turn towards the second corner goal and move into it using timeMove
+			turn(-35, true, 1, true);
+			timeMove(80, 500); //speed of 80 for 500 MS
+			timeMove(30, 400);
+
+			index(); //intaking the balls to descore while in position to shoot
+			delay(1500);
+			sis();
+			autonASNI(); //shoot NO INDEX 
+
+
+			//Move backwards and turn away from the goal and towards the front
+			//outake_ball(110, descore_corner_goals); //Outaking the ball while moving backwards
+			
+			doubleMove(-250, 50, true, 100, 100, 0.9);
+			descoreFunc(); //stop the outaking
+			turn(127, true, 1, true);
+			
+
+			//Go forward, turn, and intake the perimeter ball
+			doubleMove(340, 50, true, 100, 100, 1.3);
+			turn(-88, true, 1, true);
+			index(); 
+			timeMove(40, 1220);
+
+
+			//Move back from the perimeter, turn, and now face the ball in front of the edge goal
+			//Move towards that ball
+			doubleMove(-310, 50, true, 100, 100, 0.9);
+			turn(89, true, 1, true);
+			doubleMove(400, 50, true, 100, 100, 1.3);
+			doubleMove(450, 50, true, 25, 25, 0.9);
+			sis();
+
+
+			//Turn towards the edge goal, moving forward, and score it 
+			turn(-90, true, 1, true); //turn towards the goal
+			timeMove(45, 750); //moving towards the fourth goal
+
+
+			autonASI(); //shoot INDEX 
+
+			
+			//Move back from the goal and turn to face forward (same direction as we are standing)
+			doubleMove(-350, 50, true, 70, 70, 1.3);
+
+	
+			turn(90, true, 1, true);
+			
+			
+			//Move forward and then make a ~45 degree turn towards the last corner goal
+			doubleMove(770, 50, true, 100, 100, 1.3);
+			turn(-44, true, 1, true);
+
+
+			//Move forward for the goal, descore, and score
+			timeMove(90, 700);
+			timeMove(40, 650);
+			index(); //intaking the balls to descore while in position to shoot
+			delay(1000);
+			sis();
+			autonASNI(); 
+			doubleMove(-350, 50, true, 70, 70, 1.3);
+
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
